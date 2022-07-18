b64a589899f34ef0b39b4b111a04d7857af7f996
ErtanSidar <essoft3@gmail.com>
2022-07-18 12:58:38 +0300

    @RequestMapping

--- spring/src/main/java/com/essoft/spring/controller/HelloController.java
+++ spring/src/main/java/com/essoft/spring/controller/HelloController.java
@@ -2,13 +2,16 @@ package com.essoft.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-    //localhost:8080/hello
-    @GetMapping("/hello")
+    //localhost:8080/api/hello
+    //@GetMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String sayHello() {
         return "Hello World";
     }
