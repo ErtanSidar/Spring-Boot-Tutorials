2c517ef26175b0f74b4f29a464b396d42b2fc446
ErtanSidar <essoft3@gmail.com>
2022-07-18 13:10:25 +0300

    Mapping Annotation

--- spring/src/main/java/com/essoft/spring/controller/HelloController.java
+++ spring/src/main/java/com/essoft/spring/controller/HelloController.java
@@ -1,18 +1,31 @@
 package com.essoft.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
+    // GET  : we use to display data
+    // POST : we use to save data
+    // PUT  : we use to update data
+    //PATCH : we use it to update certain sections
+    //DELETE : we use to delete data
+
     //localhost:8080/api/hello
-    //@GetMapping("/hello")
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @GetMapping("/hello")
+    //@RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String sayHello() {
         return "Hello World";
+    }
+
+    @PostMapping("/save")
+    public String save() {
+        return "Data saved!";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "Data deleted!";
     }
 }
