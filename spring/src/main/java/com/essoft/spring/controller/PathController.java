e063e44c46ece9736f9e4837590ee255dbe6aae5
ErtanSidar <essoft3@gmail.com>
2022-07-18 13:29:00 +0300

    @RequestParam

--- spring/src/main/java/com/essoft/spring/controller/PathController.java
+++ spring/src/main/java/com/essoft/spring/controller/PathController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/myController")
-public class MyController {
+public class PathController {
 
     @GetMapping("/message/{message}")
     public String getMessage(@PathVariable String message) {
